f7a55565e22aab49fdadb603a3a2ae06eef22082
NOTNOTsergey <65647789+NOTNOTsergey@users.noreply.github.com>
2021-01-06 20:44:44 +0300

    Add files via upload

--- chto_to_tam.py
+++ chto_to_tam.py
@@ -2,15 +2,18 @@ import os
 import chto_to_tam_design as design
 import sys
 from PyQt5 import QtWidgets
-import sys
 
-cwd = os.getcwd()
-
-history = 0
-history_array = []
+def write_log(exc):
+	f = open('/home/sergey/fm/logs.txt', 'w')
+	f.write(str(exc))
+	f.close()
 
 def file_search(cwd):
-		results = [str(i)[ str(i).index("'")  + 1 : -2 ] for i in os.scandir(cwd)]
+		try:
+			results = [str(i)[ str(i).index("'")  + 1 : -2 ] for i in os.scandir(cwd)]
+		except:
+			print("it's an error")
+			return []
 
 		files = [ [], [] ]
 
@@ -27,33 +30,127 @@ def file_search(cwd):
 					files[1].append(i)
 		return files
 
+
+array = ['hide_dirs', 'hide_files']
+
 class ExampleApp(QtWidgets.QDialog, design.Ui_Dialog):
 
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
+
 		self.LineEdit.returnPressed.connect(self.set_items)
+
 		self.scripts_LineEdit.returnPressed.connect(self.set_scripts_items)
-		self.scripts_listWidget.itemPressed.connect(self.function)
 
-	def set_items(self):
-		if self.LineEdit.text() != "":
+		self.finder_LineEdit.returnPressed.connect(self.set_scripts_items)
+
+		self.listWidget.itemClicked.connect(self.selectionChanged)
+
+		self.scripts_listWidget.itemDoubleClicked.connect(self.start_program_on_python)
+
+		self.listWidget.itemDoubleClicked.connect(self.new_search)
+
+	def new_search(self, item):
+		dir_now = self.LineEdit.text() + item.text()
+		if os.path.isdir(dir_now) and not dir_now.endswith('/'):
+			dir_now += '/'
+			self.LineEdit.setText(self.LineEdit.text() + '/')
+		if os.path.isdir(dir_now):
 			self.listWidget.clear()
+			os.system('cd ' + dir_now)
+			result = file_search(dir_now)
+			self.LineEdit.setText(dir_now)
+			self.listWidget.clear()
+			self.listWidget.addItem(f'|>	{len(result[0])}				папок---------------------------------------------------------------------------------')
+			if len(result[0]) != 0:
+				if len(result[0]) >= 16:
+					self.listWidget.addItems(result[0][ : 15] + ['d---->'])
+					array[0] = 'hide_dirs'
+				else:
+					self.listWidget.addItems(result[0])
+					array[0] = 'full_dirs'
+			else:
+				self.listWidget.addItem('в этой папке нет вложенных папок')
+			self.listWidget.addItem(f'|>	{len(result[1])}				файлов--------------------------------------------------------------------------------')
+			if len(result[1]) != 0:
+				if len(result[1]) >= 16:
+					self.listWidget.addItems(result[1][ : 15] + ['f---->'])
+					array[0] = 'hide_files'
+				else:
+					self.listWidget.addItems(result[1])
+					array[1] = 'full_dirs'
+			else:
+				self.listWidget.addItem('нет файлов в этой папке')
+		#____________________________________________________функция только для линукс
+		elif os.path.isfile(dir_now) and (dir_now.endswith('.jpg') or dir_now.endswith('.ico') or dir_now.endswith('.png')):
+			os.system('gwenview ' +  dir_now)
+		#____________________________________________________
+		elif os.path.isfile(dir_now) and (dir_now.endswith('.json') or dir_now.endswith('.py')):
+			os.system('code ' + dir_now)
+		
 
+	def start_program_on_python(self, item):
+		if os.path.isfile(self.scripts_LineEdit.text() + item.text()) and item.text().endswith('.py'):
+			os.system("python -u " + '"' + self.scripts_LineEdit.text() + item.text() + '"')
+			return None	
 
-			try:
+	def selectionChanged(self, item):
+		if item.text() == 'd---->':
+			self.set_items(arg='full_dirs', arg2=array[1])
+		elif item.text() == 'f---->':
+			self.set_items(arg=array[0], arg2='full_files')
+		else:
+			self.listWidget.clearSelection()
+			if os.path.isdir(self.LineEdit.text() + item.text()):
+
+				result = file_search(self.LineEdit.text())
+				self.listWidget.addItems(result[0][ : result[0].index(item.text()) + 1] + ['	delete', '	rename'] + result[0][result[0].index(item.text()) + 1: ])
+			elif os.path.isfile(self.LineEdit.text() + item.text()):
 				result = file_search(self.LineEdit.text())
 
+	def set_items(self, arg='hide_dirs', arg2='hide_files'):
+		if self.LineEdit.text() != "":
+			if self.LineEdit.text().startswith("|>"):
+				try:
+					exec(self.LineEdit.text()[3 : ])
+				except Exception as e:
+					print(e)
+				self.LineEdit.setText("|> ")
+			elif self.LineEdit.text().startswith("||>"):
+				try:
+					os.system(self.LineEdit.text()[4 : ])
+				except Exception as e:
+					print(e)
+				self.LineEdit.setText("||> ")
+			else:
+				result = file_search(self.LineEdit.text())
 				self.listWidget.clear()
-				self.listWidget.addItem('=>					папки---------------------------------------------------------------------------------')
-				if len(result[0]) != 0:self.listWidget.addItems(result[0])
-				else:self.listWidget.addItem('в этой папке нет вложенных папок')
-
-				self.listWidget.addItem('=>					файлы---------------------------------------------------------------------------------')
-				if len(result[1]) != 0:self.listWidget.addItems(result[1])
-				else:self.listWidget.addItem('нет файлов в этой папке')
-			except Exception as e:
-				self.LineEdit.setText(str(e))
+				try:
+					self.listWidget.clear()
+					self.listWidget.addItem(f'|>	{len(result[0])}				папок---------------------------------------------------------------------------------')
+					if len(result[0]) != 0:
+						if len(result[0]) >= 16 and arg == 'hide_dirs':
+							self.listWidget.addItems(result[0][ : 15] + ['d---->'])
+							array[0] = 'hide_dirs'
+						else:
+							self.listWidget.addItems(result[0])
+							array[0] = 'full_dirs'
+					else:
+						self.listWidget.addItem('в этой папке нет вложенных папок')
+					self.listWidget.addItem(f'|>	{len(result[1])}				файлов--------------------------------------------------------------------------------')
+					if len(result[1]) != 0:
+						if len(result[1]) >= 16 and arg2 == 'hide_files':
+							self.listWidget.addItems(result[1][ : 15] + ['f---->'])
+							array[0] = 'hide_files'
+						else:
+							self.listWidget.addItems(result[1])
+							array[1] = 'full_dirs'
+					else:
+						self.listWidget.addItem('нет файлов в этой папке')
+					return
+				except Exception as e:
+					print(e)
 
 	def set_scripts_items(self):
 		try:
@@ -65,23 +162,24 @@ class ExampleApp(QtWidgets.QDialog, design.Ui_Dialog):
 			fiile = []
 
 			for files in result[1]:
-				if files.endswith('.py'):
+				if files.endswith(self.finder_LineEdit.text()):
 					fiile.append(files)
 			
 			if len(fiile) == 0:
 				self.scripts_listWidget.clear()
-				self.scripts_listWidget.addItem('файлов с расширением .py в данной папке нет')
+				self.scripts_listWidget.addItem(f'файлов с расширением {self.finder_LineEdit.text()} в данной папке нет')
 				self.scripts_listWidget.addItem('какая жалость, поищите в другой папке')
 			else:
 				self.scripts_listWidget.clear()
 				self.scripts_listWidget.addItems(fiile)
 		except Exception as e:
-			self.scripts_LineEdit.setText(str(e))			
+			print(e)			
 
-	def function(self):
-		print()
-	
-app = QtWidgets.QApplication(sys.argv)
-window = ExampleApp()
-window.show()
-app.exec_()
+
+
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	window = ExampleApp()
+	window.show()
+	app.exec_()
--- chto_to_tam_design.py
+++ chto_to_tam_design.py
@@ -10,35 +10,65 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+f = open('/home/sergey/fm/settings.txt')
+
+list_ = []
+
+for i in f:
+    list_.append(i)
+f.close()
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
+        #создаем окошко
+        #----------------------------------------------------------------- функция только для создателя (ну либо пишите свой адрес картинки)
+        Dialog.setWindowIcon(QtGui.QIcon(list_[0][0 : -1] + 'explorer.ico'))
+        #----------------------------------------------------------------- конец
         Dialog.setObjectName("not_Dialog")
         Dialog.title = "qwe"
         Dialog.resize(1920, 1080)
 
+        #создаем первую строку ввода
         self.LineEdit = QtWidgets.QLineEdit(Dialog)
         self.LineEdit.setObjectName("LineEdit")
         self.LineEdit.setGeometry(61, 1, 1000, 30)
+        self.LineEdit.setText('')
+        # self.LineEdit.setStyleSheet('background-color:#333333')
 
+        #создаем виджет который отображает результаты
         self.listWidget = QtWidgets.QListWidget(Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(0, 35, 1300, 1015))
+        self.listWidget.setGeometry(QtCore.QRect(0, 35, 1300, 1014))
         self.listWidget.setObjectName("listWidget")
-        self.listWidget.addItem('чтобы увидеть файлы в нужной папке введите её адрес')
+        self.listWidget.addItem('чтобы увидеть файлы в нужной папке введите путь к папке')
+        # self.listWidget.setStyleSheet('background-color:#333333')
 
+        #создаем вторую строку ввода
         self.scripts_LineEdit = QtWidgets.QLineEdit(Dialog)
-        self.scripts_LineEdit.setGeometry(QtCore.QRect(1305, 35, 545, 30))
+        self.scripts_LineEdit.setGeometry(QtCore.QRect(1305, 35, 443, 30))
         self.scripts_LineEdit.setObjectName("scripts_Lineedit")
+        # self.scripts_LineEdit.setStyleSheet('background-color:#333333')
 
+        self.finder_LineEdit = QtWidgets.QLineEdit(Dialog)
+        self.finder_LineEdit.setGeometry(QtCore.QRect(1750, 35, 100, 30))
+        self.finder_LineEdit.setObjectName('finder_LineEdit')
+        # self.finder_LineEdit.setStyleSheet('background-color:#111111')
+
+        #создаем второй виджет который отображает результаты после поиска из строки
         self.scripts_listWidget = QtWidgets.QListWidget(Dialog)
         self.scripts_listWidget.setObjectName("listwidget_scripts_list")
-        self.scripts_listWidget.setGeometry(QtCore.QRect(1305, 69, 545, 980))
-        self.scripts_listWidget.addItem('чтобы найти файлы .py введите адрес папки в  строку сверху ^')
-        self.scripts_listWidget.addItem('чтобы запустить файл просто введите его адрес в строку и нажмите Enter')
+        self.scripts_listWidget.setGeometry(QtCore.QRect(1305, 69, 545, 780))
+        self.scripts_listWidget.addItem('для поиска .py файлов введите в строку сверху путь')
+        self.scripts_listWidget.addItem('нажмите Enter чтобы запустить файл с расширением .py')
+        # self.scripts_listWidget.setStyleSheet('background-color:#333333')
+
+
+
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "file dispetcher"))
+        Dialog.setWindowTitle(_translate("Dialog", "file dispatcher"))
